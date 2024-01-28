ff04f2d8b2ac36a53876acfaafff3a4ac058ab52
Heem Dev <40331543+Heem-dev@users.noreply.github.com>
2024-01-28 22:00:33 +0300

    Added temp unit selecting. it's saved locally too.

--- script.js
+++ script.js
@@ -2,37 +2,81 @@ const apiKey = "9a8146eb4e564879f6aabfbe095f751d";
 const ipAPI = "https://api.ipify.org?format=json";
 
 const cityName = document.querySelector("#search_bar");
-const temp = document.querySelector('#temp')
-const clouds = document.querySelector('#clouds')
-const rain= document.querySelector('#rain')
-const humid= document.querySelector('#humid')
-const wind= document.querySelector('#wind')
-const weekforecast= document.querySelector('#weekforecast')
-let icon = document.querySelector('img#cloudicon')
-let low = document.querySelector('div.low')
-let high = document.querySelector('div.high')
-const temphighlow = document.querySelector('#temphighlow')
+const temp = document.querySelector("#temp");
+const clouds = document.querySelector("#clouds");
+const rain = document.querySelector("#rain");
+const humid = document.querySelector("#humid");
+const wind = document.querySelector("#wind");
+const weekforecast = document.querySelector("#weekforecast");
+let icon = document.querySelector("img#cloudicon");
+let low = document.querySelector("div.low");
+let high = document.querySelector("div.high");
+const temphighlow = document.querySelector("#temphighlow");
 
-const feels = document.querySelector('.feels')
+const feels = document.querySelector(".feels");
 
 let lati;
 let long;
 
+const tempformat = document.querySelector(".tempFormat");
+
+let selectedFormat = "metric";
+selectedFormat = localStorage.getItem("selectedFormat");
+
+localStorage.setItem("selectedFormat", selectedFormat);
+
+// set a local storage item for the selected format.
+
+// get the selected format from local storage.
+
+tempformat.querySelector(".cels").addEventListener("click", () => {
+  selectedFormat = "metric";
+
+  tempformat.querySelector(".cels").classList.remove("unselected");
+  tempformat.querySelector(".fahr").classList.add("unselected");
+
+  localStorage.setItem("selectedFormat", selectedFormat);
+
+  console.log(selectedFormat);
+});
+
+console.log(selectedFormat);
+
+tempformat.querySelector(".fahr").addEventListener("click", () => {
+  selectedFormat = "imperial";
+
+  tempformat.querySelector(".cels").classList.add("unselected");
+  tempformat.querySelector(".fahr").classList.remove("unselected");
+
+  localStorage.setItem("selectedFormat", selectedFormat);
+
+  console.log(selectedFormat);
+});
+
+if (selectedFormat === "metric") {
+  tempformat.querySelector(".cels").classList.remove("unselected");
+  tempformat.querySelector(".fahr").classList.add("unselected");
+}
+if (selectedFormat === "imperial") {
+  tempformat.querySelector(".cels").classList.add("unselected");
+  tempformat.querySelector(".fahr").classList.remove("unselected");
+}
+
 //fetching ip
 
-fetch(ipAPI) 
-.then((response) => response.json())
-.then((data) => {
-  console.log(data);
-  // fetching city name from ip
-  fetch('https://ipapi.co/'+data.ip+'/json/')
+fetch(ipAPI)
   .then((response) => response.json())
   .then((data) => {
     console.log(data);
-    weather(data.city)
+    // fetching city name from ip
+    fetch("https://ipapi.co/" + data.ip + "/json/")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        weather(data.city);
+      });
   });
-});
-
+let speedFormat = "km/h";
 async function weather(city) {
   //geocoding city cords
 
@@ -47,7 +91,7 @@ async function weather(city) {
       console.log(data);
       long = data[0].lon;
       lati = data[0].lat;
-    console.log(lati);
+      console.log(lati);
       console.log(long);
     });
 
@@ -55,35 +99,55 @@ async function weather(city) {
   let response = await fetch(
     "https://api.openweathermap.org/data/2.5/weather?lat=" +
       lati +
-      "&lon="+long+"&units=metric&appid=" +
+      "&lon=" +
+      long +
+      "&units=" +
+      selectedFormat +
+      "&appid=" +
       apiKey
   );
   let json = await response.json();
   console.log(json);
 
   console.log(json.main.temp);
+  if (selectedFormat === "metric") {
+    formatSeleced = "C";
+  }
+  if (selectedFormat === "imperial") {
+    formatSeleced = "F";
+  }
+  temp.textContent = Math.round(json.main.temp) + " " + formatSeleced + "°";
 
-  temp.textContent = Math.round(json.main.temp)+" C°";
+  icon.src =
+    "https://openweathermap.org/img/wn/" + json.weather[0].icon + "@2x.png";
 
-  icon.src = 'https://openweathermap.org/img/wn/'+json.weather[0].icon+'@2x.png'
-  
-  high.textContent = 'High: '+ Math.round(json.main.temp_max)
-  low.textContent = "Low: "+Math.round(json.main.temp_min)
+  high.textContent = "High: " + Math.round(json.main.temp_max);
+  low.textContent = "Low: " + Math.round(json.main.temp_min);
   // document.querySelector('#temp_container').append(icon)
   // temphighlow.append(high)
   // temphighlow.append(low)
-  
-  let description = json.weather[0].description.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
-  clouds.textContent = 'Clouds: '+description
-  icon.title = description
-  humid.textContent = 'Humidity: '+json.main.humidity+'%';
-  wind.textContent = 'Wind Speed: '+json.wind.speed+' km/h'
 
-  feels.textContent = 'Feels Like: '+Math.round(json.main.feels_like)+' C°'
+  if (selectedFormat === "imperial") {
+    speedFormat = "mph";
+  }
+  if (selectedFormat === "metric") {
+    speedFormat = "km/h";
+  }
+
+  let description = json.weather[0].description
+    .split(" ")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
+  clouds.textContent = "Clouds: " + description;
+  icon.title = description;
+  humid.textContent = "Humidity: " + json.main.humidity + "%";
+  wind.textContent = "Wind Speed: " + json.wind.speed + " " + speedFormat;
+
+  feels.textContent = "Feels Like: " + Math.round(json.main.feels_like);
 
   // document.querySelector('body').style.backgroundImage='url(https://source.unsplash.com/1920x1080/?'+city+')';
 
-cityName.placeholder = city
+  cityName.placeholder = city;
 }
 
 document.querySelector("#search_bar").addEventListener("keypress", (e) => {
